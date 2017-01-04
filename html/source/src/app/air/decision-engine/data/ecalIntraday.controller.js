728e5236f9b059ee57b48220de745e05040dd22c
cwood119 <cwood119@gmail.com>
2017-01-03 17:22:35 -0700

    Misc dashboard bug fixes.

--- html/source/src/app/air/decision-engine/data/ecalIntraday.controller.js
+++ html/source/src/app/air/decision-engine/data/ecalIntraday.controller.js
@@ -14,7 +14,7 @@
         $http.get('app/air/decision-engine/data/ecal-intraday-data.json?ts='+new Date().getTime())
         .success(function(data, status, headers) {
             vm.ecalIntraday = data;
-            vm.ecalPreLength = data.length;
+            vm.ecalIntradayLength = data.length;
             vm.ecalIntradayCurPage = 1;
             vm.ecalIntradayLimitOptions = [5,10,15];
             vm.ecalIntradayPageSize = 5;
@@ -22,15 +22,15 @@
             vm.newModified = new Date(vm.modified);
             vm.updated = vm.newModified.toLocaleString();
             vm.twoDaysAgo = moment().startOf('day').subtract(2, 'days').toDate();
-            if (vm.newModified < vm.today){vm.ecalPreToggle=0;vm.ecalPre=[];}
-            if (vm.ecalPreLength == 0) {vm.ecalPreToggle=0;}
+            if (vm.newModified < vm.today){vm.ecalIntradayToggle=0;vm.ecalIntraday=[];}
+            if (vm.ecalIntradayLength == 0) {vm.ecalIntradayToggle=0;}
         });
         $http.get('app/air/earnings-calendar/data/data.json?ts='+new Date().getTime())
         .then(function(response) {
             vm.data = response.data;
             vm.list = response.data[0].list;
-            if (vm.list == 'Calendar Movers') {vm.ecalIntradayToggle=0;}
-            if (vm.list == 'Pre Market Movers') {vm.ecalIntradayToggle=0;}
+            if (vm.list == 'Calendar Movers') {vm.ecalIntradayToggle=0;vm.ecalIntraday=[];}
+            if (vm.list == 'Pre Market Movers') {vm.ecalIntradayToggle=0;vm.ecalIntraday=[];}
         });
         // Vitals Modal
         vm.openVitals = function (e, symbol) {
--- html/source/src/app/air/decision-engine/data/gainers.controller.js
+++ html/source/src/app/air/decision-engine/data/gainers.controller.js
@@ -10,11 +10,13 @@
         vm.gainersIntraday = [];
         $http.get('app/air/decision-engine/data/gainers-intraday-data.json?ts='+new Date().getTime())
         .then(function(gainersResponse) {
+            var symbol=gainersResponse.data[0].symbol;
             vm.gainersIntraday = gainersResponse.data;
             vm.gainersList = gainersResponse.data[0].list;
             vm.gainersCurPage = 1;
             vm.gainersLimitOptions = [6,12,24];
             vm.gainersPageSize = 12;
+            if (symbol == "") {vm.gainersIntradayToggle=0;vm.gainersIntraday = [];}
         });
     }
 })();
