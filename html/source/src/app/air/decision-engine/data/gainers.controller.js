110e77af99c565474be19b3e1f401480e2c7875e
cwood119 <cwood119@gmail.com>
2017-01-11 08:18:24 -0700

    Decision Engine bug fix for empty market movers data set

--- html/source/src/app/air/decision-engine/data/gainers.controller.js
+++ html/source/src/app/air/decision-engine/data/gainers.controller.js
@@ -8,15 +8,11 @@
     function gainersDEController($interval, $http) {
         var vm = this;
         vm.gainersIntraday = [];
+        vm.gainersIntradayLength = null;
         $http.get('app/air/decision-engine/data/gainers-intraday-data.json?ts='+new Date().getTime())
-        .then(function(gainersResponse) {
-            var symbol=gainersResponse.data[0].symbol;
-            vm.gainersIntraday = gainersResponse.data;
-            vm.gainersList = gainersResponse.data[0].list;
-            vm.gainersCurPage = 1;
-            vm.gainersLimitOptions = [6,12,24];
-            vm.gainersPageSize = 12;
-            if (symbol == '') {vm.gainersIntradayToggle=0;vm.gainersIntraday = [];}
+        .success(function(data) {
+            vm.gainersIntradayLength = data.length;
+            vm.gainersIntraday = data;
         });
     }
 })();
