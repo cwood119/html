4ba873637ccd200e5a46032272c2362184be44c9
chris wood <cwood@localhost.localdomain>
2017-06-04 10:28:53 -0600

    Add Slim API

---
+++ api/index.php
@@ -0,0 +1,41 @@
+<?php
+require 'Slim/Slim.php';
+require 'mysql.php';
+
+ \Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+
+
+// all responses are in json
+$app->response->headers->set('Content-Type', 'application/json');
+
+// all requests are in json - add this so we dont have to json_decode everything
+$app->request->headers->set('Content-Type', 'application/json');
+$app->add(new \Slim\Middleware\ContentTypes());
+
+$app->get('/ecal', function () use ($app) {
+    $ecal = get_ecal();
+    if (null !== $ecal) {
+        $app->response->setStatus(200);
+        echo json_encode($ecal);
+    } else {
+        $app->response->setStatus(401);
+    }
+});
+$app->map('/', function() use($app) {
+    $response = $app->response();
+    $response->header('Access-Control-Allow-Origin', '*');
+    $response->header('Access-Control-Allow-Methods', 'GET, POST , OPTIONS');
+    $response->header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, accept, x-requested-with, origin, content-type, x-xsrf-token');
+})->via('GET', 'POST');
+
+
+// Data Functions
+function get_ecal() {
+    $pdo = connect_to_db();    
+    $data = $pdo->query('SELECT * FROM `earnings-calendar-latest`')->fetchAll();
+    return $data;
+}
+
+
+$app->run();
---
+++ api/mysql.php
@@ -0,0 +1,22 @@
+<?php
+function connect_to_db() {
+    $host = 'localhost'; // this may be an ip address instead
+    $db = 'mymedi13_air'; // name of your database
+    $user = 'cwood';
+    $pass = 'placeholder';
+    $charset = 'utf8';
+    #$connection = new mysqli($server, $user, $pass, $database);
+    
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $opt = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $pdo = new PDO($dsn, $user, $pass, $opt);
+   
+    return $pdo;
+}
+
+?>
+
